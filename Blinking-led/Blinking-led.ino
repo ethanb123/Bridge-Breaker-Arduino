e9e9b40a05273dbee6ee04931ffffd07164e6541
Ethan Bowman <ethanbowman9@gmail.com>
2024-09-05 16:04:45 -0400

    Added a second relay, and 3 more input buttons

--- Blinking-led/Blinking-led.ino
+++ Blinking-led/Blinking-led.ino
@@ -1,35 +1,111 @@
 
 // Constants for LED and switch pins
-const int ledPin = 13;  // Pin for LED
-const int switchPin = 12;  // Pin for switch
+const int Relay1 = 13;  // Pin for LED
+const int Button1 = 12;  // Pin for switch
+
+const int Button2 = 11;  // Pin for LED
+const int Relay2 = 10;  // Pin for LED
+
+const int Button3 = 9;  // Pin for LED
+const int Button4 = 8;  // Pin for LED
 
 // Variable to hold switch state
 int switchState = 0;
+int switchState2 = 0;
+int switchState3 = 0;
+int switchState4 = 0;
 
 void setup() {
   // Set the LED pin as an output
-  pinMode(ledPin, OUTPUT);
+  pinMode(Relay1, OUTPUT);
   
   // Set the switch pin as an input
-  pinMode(switchPin, INPUT_PULLUP);  // Using INPUT_PULLUP to prevent floating input
+  pinMode(Button1, INPUT_PULLUP);  // Using INPUT_PULLUP to prevent floating input
+
+  // Set the switch pin as an input
+  pinMode(Button2, INPUT_PULLUP);  // Using INPUT_PULLUP to prevent floating input
+
+  // Set the switch pin as an input
+  pinMode(Button3, INPUT_PULLUP);  // Using INPUT_PULLUP to prevent floating input
+
+  // Set the switch pin as an input
+  pinMode(Button4, INPUT_PULLUP);  // Using INPUT_PULLUP to prevent floating input
+
+  // Set the LED pin as an output
+  pinMode(Relay2, OUTPUT);
+
+  Serial.begin(9600);
 }
 
 void loop() {
   // Read the state of the switch
-  switchState = digitalRead(switchPin);
+  switchState = digitalRead(Button1);
+  switchState2 = digitalRead(Button2);
+  switchState3 = digitalRead(Button3);
+  switchState4 = digitalRead(Button4);
 
   // If the switch is pressed (LOW state)
   if (switchState == LOW) {
     // Turn on the LED
+    /*
+     Serial.print("Zero factor: ");
     for(int i=0; i<5; i++) {
-      digitalWrite(ledPin, LOW);
+      digitalWrite(Relay1, LOW);
       delay(200);
-      digitalWrite(ledPin, HIGH);
+      digitalWrite(Relay1, HIGH);
       delay(200);
-    }
+    }*/
+    digitalWrite(Relay1, LOW);
     
   } else {
     // Otherwise, turn off the LED
-    digitalWrite(ledPin, HIGH);
+    digitalWrite(Relay1, HIGH);
   }
+
+
+    // If the BUTTON2 is pressed (LOW state)
+  if (switchState2 == LOW) {
+
+    /*
+    // Turn on the LED
+     Serial.print("Zero factor: ");
+    for(int i=0; i<5; i++) {
+      digitalWrite(Relay2, LOW);
+      delay(200);
+      digitalWrite(Relay2, HIGH);
+      delay(200);
+    }*/
+    digitalWrite(Relay2, LOW);
+    
+  } else {
+    // Otherwise, turn off the LED
+    digitalWrite(Relay2, HIGH);
+  }
+
+  if (switchState3 == LOW) {
+    // Turn on the LED
+    for(int i=0; i<5; i++) {
+      digitalWrite(Relay2, LOW);
+      delay(200);
+      digitalWrite(Relay2, HIGH);
+      delay(200);
+    }
+  } else {
+    // Otherwise, turn off the LED
+    digitalWrite(Relay2, HIGH);
+  }
+
+  if (switchState4 == LOW) {
+  // Turn on the LED
+  for(int i=0; i<5; i++) {
+    digitalWrite(Relay1, LOW);
+    delay(200);
+    digitalWrite(Relay1, HIGH);
+    delay(200);
+  }
+  } else {
+    // Otherwise, turn off the LED
+    digitalWrite(Relay1, HIGH);
+  }
+
 }
