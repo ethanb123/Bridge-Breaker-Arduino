777f82e3a805182ffdf6bf549608b77c6b4bb9e9
Ethan Bowman <ethanbowman9@gmail.com>
2024-09-06 12:01:13 -0400

    Updated code to not have timing issues on the relays

--- Blinking-led/Blinking-led.ino
+++ Blinking-led/Blinking-led.ino
@@ -1,37 +1,27 @@
+const int ButtonUP = 12;
+const int ButtonDOWN = 11;
 
-// Constants for LED and switch pins
-const int Relay1 = 13;  // Pin for LED
-const int Button1 = 12;  // Pin for switch
+const int ButtonGreen = 9;
+const int ButtonRed = 8;
 
-const int Button2 = 11;  // Pin for LED
-const int Relay2 = 10;  // Pin for LED
-
-const int Button3 = 9;  // Pin for LED
-const int Button4 = 8;  // Pin for LED
+const int Relay1 = 13;
+const int Relay2 = 10;
 
 // Variable to hold switch state
-int switchState = 0;
-int switchState2 = 0;
-int switchState3 = 0;
-int switchState4 = 0;
+int switchStateUP = 0;
+int switchStateDOWN = 0;
+int switchStateGreen = 0;
+int switchStateRed = 0;
 
 void setup() {
+  // Set the switch pin as an input
+  pinMode(ButtonUP, INPUT_PULLUP);  // Using INPUT_PULLUP to prevent floating input
+  pinMode(ButtonDOWN, INPUT_PULLUP);
+  pinMode(ButtonGreen, INPUT_PULLUP); 
+  pinMode(ButtonRed, INPUT_PULLUP); 
+
   // Set the LED pin as an output
   pinMode(Relay1, OUTPUT);
-  
-  // Set the switch pin as an input
-  pinMode(Button1, INPUT_PULLUP);  // Using INPUT_PULLUP to prevent floating input
-
-  // Set the switch pin as an input
-  pinMode(Button2, INPUT_PULLUP);  // Using INPUT_PULLUP to prevent floating input
-
-  // Set the switch pin as an input
-  pinMode(Button3, INPUT_PULLUP);  // Using INPUT_PULLUP to prevent floating input
-
-  // Set the switch pin as an input
-  pinMode(Button4, INPUT_PULLUP);  // Using INPUT_PULLUP to prevent floating input
-
-  // Set the LED pin as an output
   pinMode(Relay2, OUTPUT);
 
   Serial.begin(9600);
@@ -39,50 +29,28 @@ void setup() {
 
 void loop() {
   // Read the state of the switch
-  switchState = digitalRead(Button1);
-  switchState2 = digitalRead(Button2);
-  switchState3 = digitalRead(Button3);
-  switchState4 = digitalRead(Button4);
+  switchStateUP = digitalRead(ButtonUP);
+  switchStateDOWN = digitalRead(ButtonDOWN);
+  switchStateGreen = digitalRead(ButtonGreen);
+  switchStateRed = digitalRead(ButtonRed);
 
-  // If the switch is pressed (LOW state)
-  if (switchState == LOW) {
-    // Turn on the LED
-    /*
-     Serial.print("Zero factor: ");
-    for(int i=0; i<5; i++) {
-      digitalWrite(Relay1, LOW);
-      delay(200);
-      digitalWrite(Relay1, HIGH);
-      delay(200);
-    }*/
+  // If the up switch is pressed (LOW state)
+  if (switchStateUP == LOW) {
     digitalWrite(Relay1, LOW);
-    
+    delay(1);
   } else {
-    // Otherwise, turn off the LED
     digitalWrite(Relay1, HIGH);
   }
 
-
-    // If the BUTTON2 is pressed (LOW state)
-  if (switchState2 == LOW) {
-
-    /*
-    // Turn on the LED
-     Serial.print("Zero factor: ");
-    for(int i=0; i<5; i++) {
-      digitalWrite(Relay2, LOW);
-      delay(200);
-      digitalWrite(Relay2, HIGH);
-      delay(200);
-    }*/
+  // If the ButtonDOWN is pressed (LOW state)
+  if (switchStateDOWN == LOW) {
     digitalWrite(Relay2, LOW);
-    
+    delay(1);
   } else {
-    // Otherwise, turn off the LED
     digitalWrite(Relay2, HIGH);
   }
 
-  if (switchState3 == LOW) {
+  if (switchStateGreen == LOW) {
     // Turn on the LED
     for(int i=0; i<5; i++) {
       digitalWrite(Relay2, LOW);
@@ -95,7 +63,7 @@ void loop() {
     digitalWrite(Relay2, HIGH);
   }
 
-  if (switchState4 == LOW) {
+  if (switchStateRed == LOW) {
   // Turn on the LED
   for(int i=0; i<5; i++) {
     digitalWrite(Relay1, LOW);
